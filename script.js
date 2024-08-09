24002e5a9b4b740a26514f95cb52a745506fe095
acheco <80710861+acheco@users.noreply.github.com>
2024-08-09 18:25:42 -0400

    Adding playGame function and finishing the app

--- script.js
+++ script.js
@@ -9,3 +9,66 @@ function getComputerChoice() {
     return items[randomItem];
 }
 
+function getHumanChoice() {
+    return prompt("please input your choice: ");
+}
+
+function playGame() {
+    let humanScore = 0;
+    let computerScore = 0;
+    let currentRound = 0;
+
+    function playRound(humanChoice, computerChoice) {
+        let formatedHumanChoice = humanChoice.toLowerCase();
+
+        if (formatedHumanChoice === "rock" && computerChoice === "paper") {
+            computerScore = ++computerScore;
+            return console.log("You lose! Paper beat Rock");
+        }
+
+        if (formatedHumanChoice === "paper" && computerChoice === "scissor") {
+            computerScore = ++computerScore;
+            return console.log("You lose! Scissor beat Paper");
+        }
+
+        if (formatedHumanChoice === "scissor" && computerChoice === "rock") {
+            computerScore = ++computerScore;
+            return console.log("You lose! Rock beat Scissor");
+        }
+
+        if (formatedHumanChoice === "paper" && computerChoice === "rock") {
+            humanScore = ++humanScore;
+            return console.log("You win! Paper beat Rock");
+        }
+
+        if (formatedHumanChoice === "scissor" && computerChoice === "paper") {
+            humanScore += ++humanScore;
+        }
+
+        if (formatedHumanChoice === "rock" && computerChoice === "scissor") {
+            humanScore = ++humanScore;
+            return console.log("You win! Rock beat scissor");
+        }
+
+        return console.log("It's a tie, try again!");
+
+    }
+
+    for (let i = 5; i > 0; i--) {
+        currentRound = ++currentRound
+        console.log(`Round number ${currentRound}`)
+        playRound(getHumanChoice(), getComputerChoice());
+
+    }
+
+    if (humanScore > computerScore) {
+        return console.log(`You win! your final score is ${humanScore}`)
+    } else {
+        return console.log(`You lose! computer score is ${computerScore}`)
+    }
+
+}
+
+playGame();
+
+
